b28a21eac92f13bbfa096fc31b34baf41925c1a7
jmarcusgill <jmarcusgill@gmail.com>
2017-04-20 20:38:41 -0500

    grabbing json info on click, logging one array with two objects

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,41 @@
-console.log("yoyo");
+
+$("#btn").on("click", function(){
+  let player1Val = $("#player1").val();
+  let player2Val = $("#player2").val();
+
+  const player1 = [];
+  const player2 = [];
+
+  const loadPlayer1 = () => {
+      return new Promise((resolve, reject) => {
+        $.ajax("https://teamtreehouse.com/" + player1Val + ".json")
+        .done((data1) => resolve(data1))
+        .fail((error1) => reject(error1));
+        });
+  };
+  const loadPlayer2 = () => {
+      return new Promise((resolve, reject) => {
+        $.ajax("https://teamtreehouse.com/" + player2Val + ".json")
+        .done((data2) => resolve(data2))
+        .fail((error2) => reject(error2));
+        });
+  };
+
+  Promise.all([loadPlayer1(), loadPlayer2()])
+          .then((allData) => {
+              console.log("allData", allData);
+          }).catch((errors) =>{
+            console.log(errors);
+  });
+
+
+});//closing click event
+
+
+
+
+
+
+
+
+
