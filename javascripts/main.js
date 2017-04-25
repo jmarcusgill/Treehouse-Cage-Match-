7be8d81de8fccbb964ef48ef49e1b3d84053c2c6
jmarcusgill <jmarcusgill@gmail.com>
2017-04-25 15:25:37 -0500

    some stuff printing to the dom

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,9 +2,41 @@
 $("#btn").on("click", function(){
   let player1Val = $("#player1").val();
   let player2Val = $("#player2").val();
+  let winnerEl = $("#winner");
+  let outputEl = $("#output");
 
   const players = [];
-  const outputEl = $("#output");
+  const playerTotalPoints = [];
+
+  const writeToDom = (players) => {
+    let playerDomString = "";
+    // outputEl = "";
+    for (i = 0; i < players.length; i++) {
+      playerDomString += `<div class="player row col-md-5">`;
+      playerDomString += `<img class="image img-circle" src="${players[i].gravatar_url}"`;
+      playerDomString += `<p class="points">total points: ${players[i].points.total}</p>`;
+      playerDomString += `</div>`;
+    }
+    outputEl.append(playerDomString).show("slow");
+  };
+
+  const checkForWinner = () => {
+    let winner = "";
+    let playerPointString = "";
+
+    for (var j = 0; j < players.length; j++) {
+      playerPointString = players[j].points.total;
+      playerTotalPoints.push(playerPointString);
+    }
+    if (playerTotalPoints[0] > playerTotalPoints[1]) {
+      winnerEl.val("the winner " + (players[0].name));
+      winner = players[0].name;
+    } else {
+      winnerEl.val("the winner " + (players[1].name));
+      winner = players[1].name;
+    }
+    winnerEl.append("the winner is: " + winner);
+  };
 
 
   const loadPlayer1 = () => {
@@ -28,10 +60,12 @@ $("#btn").on("click", function(){
             result.forEach((xhrResult) => {
               players.push(xhrResult);
             });
+            writeToDom(players);
+            checkForWinner(players);
             // console.log(players)
           }).catch((error) => {
-           alert("please enter a treehouse profile")
-          })
+           alert("please enter a treehouse profile");
+          });
 
 
 });//closing click event
